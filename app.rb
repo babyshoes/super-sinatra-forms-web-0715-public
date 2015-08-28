e0cd8b807ae3b3b9abb3a21ff38ac901ee51a984
babyshoes <dianecai@gmail.com>
2015-08-27 21:11:21 -0400

    all done

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :'super_hero'
+  end
 
+  # get '/team' do
+  #   @team = Team.new(params[:team])
+  #   erb :'team'
+  # end
+
+  post '/teams' do
+    @team = params[:team]
+    # binding.pry
+    # redirect '/team'
+    erb :'team'
+  end
 end
